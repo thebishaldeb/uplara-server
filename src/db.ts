a5db8d899c8b901b648bc6c03151f14fc98da536
thebishaldeb <bishaldeb656@gmail.com>
2021-06-15 18:40:21 +0530

    fixes

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,18 @@
 import knex from "knex";
 import config from "./config";
 
+const connection: any = {
+  host: config.DB_HOST,
+  port: config.DB_PORT,
+  user: config.DB_USER,
+  password: config.DB_PASS,
+  database: config.DB_NAME,
+  ssl: { rejectUnauthorized: false },
+};
+
 const knexClient = knex({
   client: "pg",
-  connection: {
-    host: config.DB_HOST,
-    port: config.DB_PORT,
-    user: config.DB_USER,
-    password: config.DB_PASS,
-    database: config.DB_NAME,
-    ssl: { rejectUnauthorized: false },
-  },
+  connection,
 });
 
 export default knexClient;
